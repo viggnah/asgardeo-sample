025d82b04e11e2a1c2ef9a68aa2843a155b756f8
pamod <pamod@wso2.com>
2023-03-30 18:12:22 +0530

    Fix cve-2019-17495

--- java/loyalty-points-calculation/loyalty-project/src/main/java/org/wso2/demo/SwaggerConfig.java
+++ java/loyalty-points-calculation/loyalty-project/src/main/java/org/wso2/demo/SwaggerConfig.java
@@ -1,37 +1,35 @@
 package org.wso2.demo;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .paths(PathSelectors.ant("/loyalty/**"))
-                .apis(RequestHandlerSelectors.basePackage("org.wso2.demo"))
-                .build()
-                .apiInfo(apiInfo());
+    public GroupedOpenApi api() {
+        return GroupedOpenApi.builder()
+                .group("loyalty-public")
+                .pathsToMatch("/loyalty/**")
+                .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Loyalty Points API", //title
-                "Calculate Loyalty Points for Given User", //description
-                "Version 1.0", //version
-                "https://wso2.com/terms", //terms of service URL
-                new Contact("Pamod Sylvester", "www.wso2.com", "pamod@wso2.com"),
-                "Apache 2.0", "https://wso2.com/license", Collections.emptyList()); // contact info
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info().title("Loyalty Points API")
+                        .description("Calculate Loyalty Points for Given User")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://loyalty.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Loyalty Wiki Documentation")
+                        .url("https://loyalty.wiki.github.org/docs"));
     }
 }
