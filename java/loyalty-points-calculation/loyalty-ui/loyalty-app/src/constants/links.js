e302b5878709bad8df8490631cbf3e9e48642826
pamod <pamod@wso2.com>
2023-04-04 19:04:32 +0530

    Make URLs configurable in Loyalty UI

---
+++ java/loyalty-points-calculation/loyalty-ui/loyalty-app/src/constants/links.js
@@ -0,0 +1,13 @@
+//Setup Asgardeo Auth React SDK
+export const AsgardeoConfig = {
+    signInRedirectURL: "http://localhost:3000",
+    signOutRedirectURL: "http://localhost:3000",
+    clientID: "sfWONkMbVmfqIaKEASDPawhty9Ia",
+    baseUrl: "https://api.asgardeo.io/t/pamodorg",
+    scope: ["openid", "groups", "profile"]
+};
+
+//Loyalty Service Host and Port
+export const Hosts = {
+    Loyalty: "http://localhost:8080"
+};
